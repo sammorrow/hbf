93ffa785ff138d6160bd6451f848475d599d348b
Sam Morrow <notsammorrow@gmail.com>
2024-10-04 21:24:05 -0400

    adds packages; some scaffolding; camera movement first pass

---
+++ Assets/Scenes/_Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Placeholder class to store player state. 
+public class Player : MonoBehaviour
+{
+
+   
+    public bool IsDead;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
