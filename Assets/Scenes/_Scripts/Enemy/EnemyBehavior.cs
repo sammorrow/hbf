d07419b25b7d79cdad4c5184cca944af61890a54
Alex Duh <alexduh@gmail.com>
2024-10-06 11:05:29 -0400

    fleshed out enemy behavior more

--- Assets/Scenes/_Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scenes/_Scripts/Enemy/EnemyBehavior.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public bool justSplit = false;
     private float health;
-    private float splitHealthThreshold = 10000; // amount of health enemy needs to be able to split and reproduce
-    private float damageHealthThreshold; // min amount of health enemy needs to be able to start attacking body
+    private float splitHealthThreshold = 100; // amount of health enemy needs to be able to split and reproduce
+    private float damageHealthThreshold = 25; // min amount of health enemy needs to be able to start attacking body
     private float deadTime = 30; // how long the enemy will remain dead/removable for
     private float deadTimer;
+    public float ATTACK_RATE = 5;
+    public float DAMAGE_VALUE = 1;
+    public float REGENERATION = .5f;
+    private float attackCooldown;
 
     [SerializeField] GameObject selfPrefab; // used when enemy splits/duplicates
 
@@ -22,7 +27,8 @@ public class EnemyBehavior : MonoBehaviour
     {
         Vector3 randomVelocity = new Vector3(0,0,0); // TODO: set the newly created copy to have this velocity
         Quaternion randomRotation = Quaternion.identity; // TODO: set this to be a random rotation (only 1 axis needs to be set probably)
-        Instantiate(selfPrefab); // TODO: split the enemy (create new copy) and set its health = damageHealthThreshold
+        GameObject newEnemy = Instantiate(selfPrefab); // TODO: split the enemy (create new copy) and set its health = damageHealthThreshold
+        newEnemy.GetComponent<EnemyBehavior>().justSplit = true;
         SetHealth(damageHealthThreshold);
     }
 
@@ -35,13 +41,29 @@ public class EnemyBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (justSplit)
+        {
+            health = damageHealthThreshold;
+            justSplit = false;
+        }
+
         if (health > 0)
         {
-            // TODO: if not dead (health > 0), increase health every few game ticks
+            // if not dead (health > 0), increase health every few game ticks, then change size
+            health += REGENERATION * Time.deltaTime;
+            float enemySize = 2 * health / splitHealthThreshold;
+            transform.localScale = new Vector3(enemySize, enemySize, 1);
         }
         if (health > damageHealthThreshold)
         {
-            // TODO: if (health > damageHealthThreshold), deal damage to body (player) every few game ticks
+            // if (health > damageHealthThreshold), deal damage to body (player) every damage interval
+            if (attackCooldown <= 0)
+            {
+                Player.Instance.DamagePlayer(DAMAGE_VALUE); // TODO: multiply damage by zone bonus
+                attackCooldown = ATTACK_RATE;
+            }
+
+            attackCooldown -= Time.deltaTime / ATTACK_RATE;
         }
         if (health <= 0)
         {
@@ -51,11 +73,10 @@ public class EnemyBehavior : MonoBehaviour
             {
                 health = 1;
                 deadTimer = 0;
+                // if dead AND deadTime >= deadTimer, set health to 1 (resurrect)
             }
         }
             
-
-        // TODO: if dead AND deadTime >= deadTimer, set health to 1
         if (health >= splitHealthThreshold)
             Split();
     }
