f052a1ca07a20394962f7d212bf0d71351f2e794
Sam Morrow <notsammorrow@gmail.com>
2024-10-07 15:24:53 -0400

    ui update

--- Assets/Scenes/_Scripts/UI/UIAmmo.cs
+++ Assets/Scenes/_Scripts/UI/UIAmmo.cs
@@ -10,6 +10,11 @@ public class UIAmmo : MonoBehaviour
     public TMP_Text textAmmoCount2;
     public TMP_Text textAmmoCount3;
 
+    private void FixedUpdate()
+    {
+        UpdateUIAmmo();
+    }
+
     public void UpdateUIAmmo()
     {
         textAmmoCount1.text = Player.Instance.ammo[0].ToString();
