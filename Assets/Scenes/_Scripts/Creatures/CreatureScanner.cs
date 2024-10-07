d2cd71d04f76d52c5220b51ce4d710ec6722b043
Sam Morrow <notsammorrow@gmail.com>
2024-10-07 12:44:27 -0400

    fixing up alarms; fixing dead sprite

--- Assets/Scenes/_Scripts/Creatures/CreatureScanner.cs
+++ Assets/Scenes/_Scripts/Creatures/CreatureScanner.cs
@@ -5,30 +5,44 @@ public class CreatureScanner : CreatureBase
 {
 
     public float scanRange = 10;
-
+    public float ALARM_CYCLE = 1;
     public LayerMask enemyLayer;
+    private bool _enemyAround = false;
 
     void Start()
     {
         Lifespan = 8;
+        if (IsEnemyDetected())
+            _enemyAround = true;
+            StartCoroutine(Alarm());
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Alarm();
+        if (IsEnemyDetected())
+            StartCoroutine(Alarm());
     }
 
     void Update()
     {
-        if (Alarm())
-            ZoneManager.Instance.OnVirusDetected(gameObject.transform.position);
+        if (!IsEnemyDetected())
+            _enemyAround = false;
     }
 
-    public bool Alarm()
+    public bool IsEnemyDetected()
     {
         return Physics.CheckSphere(transform.position, scanRange, enemyLayer);
     }
 
+    IEnumerator Alarm()
+    {
+        while (_enemyAround)
+        {
+            Debug.Log("ALARM!!");
+            yield return new WaitForSeconds(ALARM_CYCLE);
+        }
+    }
 
 }
 
